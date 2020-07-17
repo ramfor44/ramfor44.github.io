7259c98f1be44aac09b70e707c369555ca233eb7
ram kumar <53056831+ramfor44@users.noreply.github.com>
2020-07-17 11:32:30 +0530

    Add files via upload

---
+++ html1.js
@@ -0,0 +1,21 @@
+window.alert("Welcome to bettle Educations...");
+            function myFun(){
+                var txt = "HTML = HyperText Markup Language";
+                document.getElementById("text").innerHTML = txt;
+                }
+            function myFun1(){
+                var txt = "Six Levels of Heading tags: h1,h2,h3,h4,h5,h6";
+                document.getElementById("text1").innerHTML = txt;
+                }
+            function myFun2(){
+                var txt = "Paragraph tag: p";
+                document.getElementById("text2").innerHTML = txt;
+                }
+            function myFun3(){
+                var txt = "Link tag: l;Attribute: href";
+                document.getElementById("text3").innerHTML = txt;
+                }
+            function myFun4(){
+                var txt = "Image tag: img;Attribute: src ,alt";
+                document.getElementById("text4").innerHTML = txt;
+                }
